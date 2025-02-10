23ad4ef5397f7b5fb61290bda18e68343234da98
JuWunpark <1116milk@gmail.com>
2025-02-10 15:30:32 +0900

    juwon2

--- new3/settings.py
+++ new3/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+# asjdfhkjshdf
 ROOT_URLCONF = "new3.urls"
 
 TEMPLATES = [
